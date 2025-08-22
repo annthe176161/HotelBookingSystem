3c0ae9e470718463d117c9560a41a8c3d3650ff3
wvrtungduong <145447160+wvrtungduong@users.noreply.github.com>
2025-08-22 22:26:43 +0700

    merge huy

---
+++ HotelBookingSystem/Migrations/20250822152530_ChangeUserModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "AspNetUsers",
+                newName: "GenderType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GenderType",
+                table: "AspNetUsers",
+                newName: "Gender");
+        }
+    }
+}
